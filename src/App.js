37c8baefa568e5f640b03a8745284227db2a6e5d
archana <archanakamath94@gmail.com>
2022-04-15 17:37:49 -0700

    testing cloudformation

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - archana
+          Learn React - Archana - updated changes
         </a>
       </header>
     </div>
